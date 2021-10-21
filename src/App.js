12527e2c16818e79a69eb108e3cc202de4a69099
Theo Khayat <theokhayat23@gmail.com>
2021-10-21 15:48:07 -0500

    add source React

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import Slider from 'react-slick';
+
+const divFromPhoto = (photo) => {
+  let url = photo.url,
+    sender = photo.sender.slice(photo.sender.length-4),
+    receivedAt = new Date(photo.receivedAt * 1000).toString();
+    // urlType = photo.urlType;
+
+    return (
+      <div key={'div_' + url}>
+        <img
+          src={url}
+          key={url}
+          alt={'alt_' + url}
+          width={'85%'}
+          height={'75%'}
+        />
+        <div key={'sender_' + sender}>{sender}</div>
+        <div key={'timed_' + receivedAt}>{receivedAt}</div>
+      </div>
+    );
+};
+
+
+class MySlider extends React.Component {
+
+  settings = { // https://react-slick.neostack.com/docs/api/
+    autoplay: true,
+    dots: false,
+    infinite: true,
+    speed: 5000,
+    autoplayspeed: 2000,
+    slidesToShow: 2,
+    slidesToScroll: 1
+  };
+
+  state = { photos: null };
+
+  componentDidMount(){
+    fetch('https://y5gfm8ypt6.execute-api.us-east-1.amazonaws.com/default/weddingPhoto')
+      .then(response => response.json())
+      .then(response => this.setState({photos: response}))
+      .catch(error => console.log('e >>', error));
+  };
+
+  render() {
+    if (!this.state.photos) {
+      return <p>Getting photos...</p>
+    } else {
+      return (
+        <Slider {...this.settings}>
+          { this.state.photos.map(photo => divFromPhoto(photo)) }
+        </Slider>
+      );
+    }; 
+  };
+
+};
+
+
+function App() {
+  return <MySlider />
+};
+
+export default App;
