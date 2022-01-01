a9e85a82102dbf19186bbedaaa0d5cff582c751d
Theo Khayat <theokhayat23@gmail.com>
2021-12-31 23:15:56 -0600

    ratios and bold <3

--- src/App.js
+++ src/App.js
@@ -4,40 +4,33 @@ import Slider from 'react-slick';
 const divFromPhoto = (photo) => {
   let url = photo.url,
     sender = photo.sender,
-    vw = 31,
-    // vh = 91;
     receivedAt = new Date(photo.receivedAt).toLocaleTimeString();
     // urlType = photo.urlType;
 
     return (
-      <div key={'div_' + url}>
+      <div
+        key={'div_' + url}
+        contentAlign='center' // Logan halp here!!! <3
+      >
         <img
           src={url}
           key={url}
           alt={'alt_' + url}
           style={{
-            width: `${vw}vw`,
-            height: `${2.71*vw}vh`
-          }}
-          onLoad={e => {
-            let natHeight = e.target.naturalHeight,
-              natWidth = e.target.naturalWidth,
-              newHeight = null,
-              newWidth = null;
-            if (natHeight > natWidth) { // portrait
-              console.log('portrait', natHeight, natWidth);
-              // newWidth = `${vw*(natWidth/natHeight)}vw`;
-              // console.log('newWidth:', newWidth);
-              // e.target.style.width = newWidth; // natWidth * (currentHeight/natHeight);
-            } else { // landscape
-              console.log('landscape', natHeight, natWidth);
-              newHeight = `${vw*(natHeight/natWidth)}vh`;
-              // console.log('newHeight:', newHeight);
-              e.target.style.height = newHeight; // natHeight * (currentWidth/natWidth);
-            };
+            width: '31vw',
+            height: 'auto',
+            maxWidth: '31vw',
+            maxHeight: '91vh'
           }}
         />
-        <div key={'sender_' + sender}>{`${sender} @ ${receivedAt}`}</div>
+        <div
+          key={'sender_' + sender}
+          style={{
+            textAlign: 'center',
+            fontSize: '24px'
+          }}>
+            {<b>{sender}</b>}{` @ ${receivedAt}`}
+        </div>
       </div>
     );
 };
